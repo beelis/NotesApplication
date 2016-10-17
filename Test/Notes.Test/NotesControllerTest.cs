f1efbf39a8cd32b70fc1259fb6ac5b68945377af
Christoph Buchli <cbuchli@snowflake.ch>
2016-10-17 19:03:54 +0200

    Working but not testing yet.

---
+++ Test/Notes.Test/NotesControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Notes.Controllers;
+using Notes.Data;
+using Notes.Models;
+using Xunit;
+
+namespace Notes.Test
+{
+    public class OrderControllerTest
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private ApplicationDbContext _dbContext;
+        private readonly ILogger _logger;
+
+
+        public OrderControllerTest()
+        {
+            var services = new ServiceCollection();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Notes.Test"));
+            _serviceProvider = services.BuildServiceProvider();
+            _dbContext = _serviceProvider.GetService<ApplicationDbContext>();
+
+        }
+
+
+        [Fact]
+        public void CreateSimple()
+        {
+            var controller = new NotesController(null, null, null);
+            Assert.NotNull(controller.Create());
+        }
+    }
+}
